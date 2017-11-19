25ad10c51c710bf55b27b01415ca7cc6c1b9a0c0
Yish <mombuartworks@gmail.com>
2017-11-19 15:50:49 +0800

    add

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,3 +5,12 @@
         <h1>{{ $user->name }}</h1>
     </div>
 @endsection
+
+@section('script')
+    <script>
+        axios.get('users/'+ '{{ auth()->id() }}' +'/achievements')
+            .then(function(res) {
+
+            });
+    </script>
+@endsection
